10725b5fded16afc9f01b7d1170ea7129fc0abd9
princetechn0 <13923942+princetechn0@users.noreply.github.com>
2019-12-20 14:23:32 -0800

    Initial Commit

---
+++ fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fun
+//
+//  Created by Ali Ataya on 12/20/19.
+//  Copyright © 2019 Ali Ataya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
